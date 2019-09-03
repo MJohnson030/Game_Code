6bc51d917542ba200280695d435acc10ceb88737
Kenzie <mbaker880@west-mec.org>
2019-09-03 16:43:08 -0700

    Commit#1
    Linked images, started on first floor story guide, next step is to manipulate the alerts to the center of the page.

---
+++ game.js
@@ -0,0 +1,39 @@
+"use strict";
+
+// global variables for multipurpose uses
+let i = "Investigate";
+let l = "Left";
+
+var enterCabin = confirm("You come across an abandoned cabin in the woods. Do you enter?");
+
+if (enterCabin == true) {
+	// option to search around the room
+  var figurineDirection = prompt("You find yourself in a cozy living room filled with tiny G. I. Joe figurines. Investigate them or look around the room (Left or Right)?");
+
+  if (figurineDirection == l) {
+    // you have entered the kitchen
+    var kitchenOptions = prompt("You have entered the kitchen, do you want to investigate?");
+
+    if (kitchenOptions == i) {
+      //found something something in the kitchen
+      var weaponOne = confirm("You found a mousetrap! Grab it?");
+
+      if (weaponOne == true) {
+        //collect and skirt
+        var mouseYes = confirm("You pocketed the mousetrap. Go back to the living room?");
+
+        if (mouseYes == true) {
+          // return to living room, new decision
+          var directionTwo = prompt("You have returned to the living room. Do you investigate the right corridor?");
+        }
+        else {
+          // hahaha wrong choice again
+          alert("As you wait in the kitchen, the crackling of something cooking startles you. As you turn around, a shadow from behind suffocates you with cheese. You died!");
+        }
+      }
+    }
+  }
+} else {
+	//if they chicken out
+  alert("A figurine flew out of the cabin and hit you in the head. You Died!");
+}
